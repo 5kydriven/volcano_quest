7026bbfe0a0a81907780d6270577dd34d9fbff90
5kydriven <tagpuno619@gmail.com>
2026-05-17 21:51:42 +0800

    first commit

---
+++ TODO.md
@@ -0,0 +1,38 @@
+### Phase 1
+
+- [] download all images
+- [] download all sfx and bg music
+- [] download all 3d models
+- [] create player model
+- [] create reusable scripts
+- [] create start page
+- [] create onboarding page
+- [] create map page for all 9 levels
+
+### Phase 2
+
+- [] create level 1 scene
+- [] create level 2 scene
+- [] create level 3 scene
+- [] create level 4 scene
+- [] create level 5 scene (3D modeling)
+
+### Phase 3
+
+- [] vfx effect for level 5
+- [] create level 6 scene
+- [] create level 7 scene
+- [] create level 8 scene
+- [] create level 9 scene
+
+### Phase 4
+
+- [] polish exp and progress
+- [] badge system
+- [] leaderboard
+- [] testing
+- [] performance optimization
+
+### Phase 5
+
+- [] deploy
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audio_session
+import just_audio
+import rive_native
+import shared_preferences_foundation
+import url_launcher_macos
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  RiveNativePlugin.register(with: registry.registrar(forPlugin: "RiveNativePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+}
