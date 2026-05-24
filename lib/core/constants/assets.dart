91ba979a9541ab3e04f07186197e83286c75a3de
5kydriven <tagpuno619@gmail.com>
2026-05-24 13:11:22 +0800

    feat: add assets

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -1,3 +1,12 @@
 class Assets {
   static const String splashBg = 'assets/splash_bg.png';
+  static const String analystBoy = 'assets/avatars/Analyst_boy.png';
+  static const String chemistGirl = 'assets/avatars/Chemist_girl.jpeg';
+  static const String explorerGirl = 'assets/avatars/Explorer_girl.png';
+  static const String geologistBoy = 'assets/avatars/Geologist_boy.jpeg';
+  static const String observerBoy = 'assets/avatars/Observer_boy.jpeg';
+  static const String researcherGirl = 'assets/avatars/Researcher_girl.png';
+  static const String scientistBoy = 'assets/avatars/Scientist_boy.jpeg';
+  static const String strategistGirl = 'assets/avatars/Strategist_girl.jpeg';
+  static const String volcanoBg = 'assets/bg/volcano.mp4';
 }
